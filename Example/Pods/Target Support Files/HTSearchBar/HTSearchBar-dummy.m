16576f576f681cc8f25d545dfbc90630f983c141
万海堂 <wanhaitang@sinter.cn>
2018-12-24 13:50:17 +0800

    “init”

---
+++ Example/Pods/Target Support Files/HTSearchBar/HTSearchBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HTSearchBar : NSObject
+@end
+@implementation PodsDummy_HTSearchBar
+@end
---
+++ Example/Pods/Target Support Files/Pods-HTSearchBar_Example/Pods-HTSearchBar_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HTSearchBar_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HTSearchBar_Example
+@end
